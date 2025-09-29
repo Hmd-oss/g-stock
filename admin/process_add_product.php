e7ea9f3e854e32f9d91720c2a19395114b8bb00f
Hamid <hamidabdoul707@gmail.com>
2025-09-29 20:40:35 +0200

    add product, process add product et product modification

--- admin/process_add_product.php
+++ admin/process_add_product.php
@@ -1,40 +1,52 @@
 <?php
 include("../database/connexion.php");
-include("../fonctions/fonction.php");
+
 
 $error = "";
 $success = "";
 
-if (isset($_POST["save_product"])){
+if (isset($_POST["add_products"])){
     $uuid = generate_uuid_v4();
     $code = $_POST["code"] ?? null;
     $name = $_POST["name"] ?? null;
     $qte = $_POST["qte"] ?? null;
+    $qte_seuil = $_POST["qte_seuil"] ?? null;
     $pu_a = $_POST["pu_a"] ?? null;
     $pu_v = $_POST["pu_v"] ?? null;
     $fabricated_at = $_POST["fabricated_at"] ?? null;
     $expired_at = $_POST["expired_at"] ?? null;
+    $fournisseur_uuid = $_POST["fournisseur_uuid"] ?? null;
+    $category_product_uuid = $_POST["category_product_uuid"] ?? null;
     
 
     // verification de l'unicite des donnees
 
-    $exist_info = $connexion->prepare("SELECT COUNT(*) FROM tlbl_products WHERE is_deleted = 1 AND( code = :code OR qte = :qte OR qte_seuil = :qte_seuil OR pu_a = :pu_a OR pu_v = :pu_v OR fabricated)");
+    $exist_info = $connexion->prepare("SELECT COUNT(*) FROM tlbl_products WHERE is_deleted = 1 AND(code = :code OR name = :name)");
     $exist_info->execute([
         ":name" => $name,
+        ":code" => $code,
     ]);
     if($exist_info->fetchColumn() > 0){
         $error = "ce produit existe deja";
+        
     }else {
         $insert_info = $connexion->prepare(
-            "INSERT INTO tlbl_products(uuid,name,description)
-            VALUES (:uuid,:name,:description)
+            "INSERT INTO tlbl_products(uuid,code,name,qte,pu_a,pu_v,qte_seuil,category_product_uuid,fournisseur_uuid,fabricated_at,expired_at)
+            VALUES (:uuid,:code,:name,:qte,:pu_a,:pu_v,:qte_seuil,:category_product_uuid,:fournisseur_uuid,:fabriated_at,:expired_at)
             ");
-
 
         $insert_info->execute([
             ":uuid" => $uuid,
-            ":name" => $name,
-            "description" => $description,
+            ":code" => $code,
+            "name" => $name,
+            "qte" => $qte,
+            "pu_a" => $pu_a,
+            "pu_v" => $pu_v,
+            "qte_seuil" => $qte_seuil,
+            "category_product_uuid" => $category_product_uuid,
+            "fournisseur_uuid" => $fournisseur_uuid,
+            "fabriated_at" => $fabricated_at,
+            "expired_at" => $expired_at
         ]);
 
         if($insert_info){
